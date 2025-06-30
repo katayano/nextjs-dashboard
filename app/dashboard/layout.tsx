b668b94398e16fbe05964d4c4bba5e0686ed76c9
katayama <idnlnroocwetn22@gmail.com>
2025-06-30 22:24:14 +0900

    Chapter 10終了

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,8 @@
 // /dashboard下にあるpagesのレイアウトを定義するファイル
 import SideNav from "@/app/ui/dashboard/sidenav";
 
+export const exprimental_ppr = true;
+
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
