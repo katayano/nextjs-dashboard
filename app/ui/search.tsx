8f1216530114f0a019d20feec5c7b700b7bc2da2
katayama <idnlnroocwetn22@gmail.com>
2025-07-06 20:18:28 +0900

    Chapter 11終了

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -15,6 +15,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     console.log(`Searching... ${term}`);
 
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1'); // Reset to the first page on new search
 
     if (term) {
       params.set('query', term);
