18220bf33000fa88442a40e137f6b1273fb9ef1b
katayama <idnlnroocwetn22@gmail.com>
2025-07-14 01:55:46 +0900

    createInvoiceのvalidationを追加

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -9,38 +9,74 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
+    customerId: z.string({
+        // invalid_type_errorはzodのv4ではなくなった
+        // invalid_type_error: 'Please select a customer',
+        error: 'Please select a customer',
+    }),
     // coerceで型検証しながら数値に変換
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    amount: z.coerce
+        .number()
+        .gt(0, {
+            message: 'Please enter an amount greater than $0',
+        }),
+    status: z.enum(['pending', 'paid'], {
+        // invalid_type_error: 'Please select an invoice status',
+        error: 'Please select an invoice status',
+    }),
     date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
-    const { customerId, amount, status } = CreateInvoice.parse({
+export type State = {
+    errors?: {
+        customerId?: string[];
+        amount?: string[];
+        status?: string[];
+    };
+    message?: string | null;
+}
+
+export async function createInvoice(prevState: State, formData: FormData) {
+    // Validate form using Zod
+    const validatedFields = CreateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
-    const amountIntoCents = amount * 100;
-    const date = new Date().toISOString().split('T')[0];
 
-    try {
-        await sql`
-        INSERT INTO invoices (customer_id, amount, status, date)
-        VALUES (${customerId}, ${amountIntoCents}, ${status}, ${date})
-    `;
-    } catch (error) {
-        console.error('Error creating invoice:', error);
+    // If form validation fails, return errors early. Otherwise, continue.
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Create Invoice.',
+        };
     }
 
-    // 更新されたデータを取得
+    // Prepare data for insertion into the database
+    const { customerId, amount, status } = validatedFields.data;
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
+
+    // Insert data into the database
+    try {
+        await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+    } catch (error) {
+        // If a database error occurs, return a more specific error.
+        return {
+            message: 'Database Error: Failed to Create Invoice.',
+        };
+    }
+
+    // Revalidate the cache for the invoices page and redirect the user.
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
-};
+}
 
 export async function updateInvoice(id: string, formData: FormData) {
     const { customerId, amount, status } = UpdateInvoice.parse({
