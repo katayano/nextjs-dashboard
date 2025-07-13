026f749a9da534ecf57986fda54b3afb0e9b859f
katayama <idnlnroocwetn22@gmail.com>
2025-07-14 02:44:11 +0900

    Chapter 14終了

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -69,7 +69,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
     } catch (error) {
         // If a database error occurs, return a more specific error.
         return {
-            message: 'Database Error: Failed to Create Invoice.',
+            message: 'Database Error: Failed to Create Invoice: ' + error,
         };
     }
 
@@ -78,12 +78,20 @@ export async function createInvoice(prevState: State, formData: FormData) {
     redirect('/dashboard/invoices');
 }
 
-export async function updateInvoice(id: string, formData: FormData) {
-    const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+    const validatedFields = UpdateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
+
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Update Invoice.',
+        };
+    }
+    const { customerId, amount, status } = validatedFields.data;
     const amountIntoCents = amount * 100;
 
     try {
@@ -93,7 +101,9 @@ export async function updateInvoice(id: string, formData: FormData) {
         WHERE id = ${id}
     `;
     } catch (error) {
-        console.error('Error updating invoice:', error);
+        return {
+            message: 'Database Error: Failed to Update Invoice: ' + error,
+        };
     }
 
     revalidatePath('dashboard/invoices');
