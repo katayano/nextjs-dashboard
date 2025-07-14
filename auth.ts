06dfc7d4171937e026eb85dbba1073d136c1ae02
katayama <idnlnroocwetn22@gmail.com>
2025-07-15 01:14:46 +0900

    Adding the sign in functionality まで実装

--- auth.ts
+++ auth.ts
@@ -1,6 +1,21 @@
 import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
+import Credentials from 'next-auth/providers/credentials';
+import { email, z } from 'zod';
 
 export const { auth, signIn, signOut } = NextAuth({
     ...authConfig,
+    // Add Credentials provider for username/password authentication
+    // 他にもOAuthやemail認証なども追加できる
+    providers: [Credentials({
+        async authorize(credentials) {
+            const parsedCredentials = z
+                .object({
+                    email: z.string().email(),
+                    password: z.string().min(6)
+                })
+                .safeParse(credentials);
+        },
+    }),
+    ],
 });
